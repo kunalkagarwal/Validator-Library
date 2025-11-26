671b49c8268d4d86be824f8136dd4bb47b352a2a
kunalkagarwal <kunalagarwal611@gmail.com>
2025-11-26 13:36:16 +0530

    chore: ignore Stryker sandbox in Jest (jest.config.js)

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   testEnvironment: 'node',
   testMatch: ['**/tests/**/*.test.js'],
-  testTimeout: 20000, // guard for slow machines
-  // If some tests are ESM or require transforms, add them here
-  // transform: {...}
+  testTimeout: 20000,
+  testPathIgnorePatterns: ['<rootDir>/.stryker-tmp/']
 };
